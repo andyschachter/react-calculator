6bffcfdd0aa621321e0e9840105e09b0c83cf835
Andy Schachter <andy.schachter@gmail.com>
2021-06-02 21:05:27 -0400

    added inputs for calculator

--- react-calc/src/components/calculator.js
+++ react-calc/src/components/calculator.js
@@ -14,10 +14,6 @@ const Calculator = () => {
     setNum1(event.target.value)
   }
 
-  const updateOperator = (event) => {
-    setOper(event.target.value)
-  }
-
   const updateNum2 = (event) => {
     setNum2(event.target.value)
   }
@@ -25,6 +21,18 @@ const Calculator = () => {
   return (
     <div className="calculator">
       <h1>React Calculator</h1>
+      <div>
+        <input type="number" name="numberOne" onChange={updateNum1} />
+        <select id= "operators">
+          <option value="plus">+</option>
+          <option value="minus">-</option>
+          <option value="multiply">*</option>
+          <option value="divide">/</option>
+        </select>
+        <input type="number" name="numberTwo" onChange={updateNum2} />
+        <input type="button" value="=" onclick=""/>
+        <input type="number" id="result" onChange={setResult}/>
+      </div>
     </div>
   )
 
