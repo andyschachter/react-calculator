62186a7e85d2cbc61a60cd312e2a0f8c27e92e3a
Andy Schachter <andy.schachter@gmail.com>
2021-06-02 20:52:10 -0400

    added header to return function

--- react-calc/src/components/calculator.js
+++ react-calc/src/components/calculator.js
@@ -2,6 +2,32 @@ import React, { useState } from 'react'
 
 const Calculator = () => {
 
+  const [num1, setNum1] = useState('')
+
+  const [oper, setOper] = useState('+')
+
+  const [num2, setNum2] = useState('')
+
+  const [result, setResult] = useState('')
+
+  const updateNum1 = (event) => {
+    setNum1(event.target.value)
+  }
+
+  const updateOperator = (event) => {
+    setOper(event.target.value)
+  }
+
+  const updateNum2 = (event) => {
+    setNum2(event.target.value)
+  }
+
+  return (
+    <div className="calculator">
+      <h1>React Calculator</h1>
+    </div>
+  )
+
 }
 
 export default Calculator
